6e78c3b371f69f102d0b7630134099dec7695b32
bodaymaryy786 <bodaymaryy786@gmail.com>
2025-03-05 19:57:39 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,8 @@
+
+<?php
+// Generating a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// Displaying the generated number on the screen
+echo "The generated number is: $randomNumber";
+?>
