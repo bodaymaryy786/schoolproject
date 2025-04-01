9e6eae3c5b2a8e242df742c05949ddec38ad1139
bodaymaryy786 <bodaymaryy786@gmail.com>
2025-04-01 08:03:52 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,15 @@
+<?php
+$students = [
+  "student1" => [
+    "name" => "Alice",
+    "age" => 20,
+    "subjects" => ["Math", "English", "Science"]
+  ],
+  "student2" => [
+    "name" => "Bob",
+    "age" => 18,
+    "subjects" => ["Math", "History", "Art"]
+  ],
+  // Add more students and their subjects as needed
+];
+?>
