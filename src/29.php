317ed38329db645758d57b2bf8f6cb1d14ade512
bodaymaryy786 <bodaymaryy786@gmail.com>
2025-04-08 05:17:55 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = 42;
+
+// Use variable values in an if statement
+if ($var1 == $var2) {
+    echo "The two values are equal.";
+} else {
+    echo "The two values are not equal.";
+}
+?>
