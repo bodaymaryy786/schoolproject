88955af074ad54d99d5739a5d5008b8229cf976a
bodaymaryy786 <bodaymaryy786@gmail.com>
2025-05-19 09:16:09 +0000

    Add 39.php

---
+++ src/39.php
@@ -0,0 +1,12 @@
+<?php
+// Example code to demonstrate basic PHP concepts
+
+// Define a variable
+$myVariable = "Hello, World!";
+
+// Output the value of $myVariable
+echo "The value of myVariable is: " . $myVariable;
+
+// Print this message
+print "Hello, world!";
+?>
